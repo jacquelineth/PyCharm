90ee055f3abefd1e361039fdb347308d51233d3e
jacquelineth <jacquelineth@gmail.com>
2019-12-14 15:12:32 +0100

    Access file RW

--- input.py
+++ input.py
@@ -1,4 +1,7 @@
 import json
 with open('input.json','r') as input:
     obj = json.load(input)
-    print('Hello, '+ obj['name'])
+    with open('output.txt', 'w') as output:
+        output.write(obj['name']+"'s Hobbies:\n")
+        for hobby in obj['hobbies'] :
+            output.write(hobby +"\n")
