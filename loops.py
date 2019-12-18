95a35ed6daabe4ba9f9782b74c518e4ff7a688d2
Thierry JACQUELINE <jacqueline@gmail.com>
2019-12-18 02:38:59 +0100

    loop into a list or  continue

--- loops.py
+++ loops.py
@@ -1,7 +1,8 @@
 student_names = ["James", "Katarina", "Jessica", "Mark", "Bort", "Frank Grimes", "Max Power" ]
 
 for name in student_names:
-    if name == "Mark":
+    if name == "Bort":
+        continue
         print("Found him! " + name)
-        break
+
     print("Currently testing " + name)
