c87e5ea5698a8e15997305f3f92b2e6159d5967a
jacquelineth <jacquelineth@gmail.com>
2019-12-18 17:25:12 +0100

    ExerciceFunction

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase =[]
     for student in students:
-        students_titlecase = student["name"].title()
+        students_titlecase.append( student["name"].title())
     return students_titlecase
 
 
@@ -14,14 +14,23 @@ def print_students_titlecase():
 
 #default arg
 def add_student(name, student_id =332) :
-    student = {"name": name, "student_id": student_id}
-    students.append(student)
+    xstudent = {"name": name, "student_id": student_id}
+    students.append(xstudent)
 
 
 student_list = get_students_titlecase()
+end = False 
+res = True
 
-student_name = input("Enter student name: " )
-student_id = input("Enter student ID: " )
+while res:
+    resChar= input("Add Student( y/n) ?" )
+    if resChar == "y" or resChar == "y":
+        student_name = input("Enter student name: " )
+        student_id = input("Enter student ID: " )
+        add_student(student_name, student_id)
+    else:
+        res= False
 
-add_student(student_name, student_id)
+
+
 print_students_titlecase()
