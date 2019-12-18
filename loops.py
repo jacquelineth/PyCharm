c9758913d2ce22852871938ac0739d6941456ef8
Thierry JACQUELINE <jacqueline@gmail.com>
2019-12-18 02:36:58 +0100

    loop into a list an break when found

--- loops.py
+++ loops.py
@@ -3,5 +3,5 @@ student_names = ["James", "Katarina", "Jessica", "Mark", "Bort", "Frank Grimes",
 for name in student_names:
     if name == "Mark":
         print("Found him! " + name)
-
+        break
     print("Currently testing " + name)
