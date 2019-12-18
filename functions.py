ce08e57fc6795b9ca2655f81882a9c907caeab0c
jacquelineth <jacquelineth@gmail.com>
2019-12-18 17:04:34 +0100

    Basic Input

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ students = []
 def get_students_titlecase():
     students_titlecase =[]
     for student in students:
-        students_titlecase = student.title()
+        students_titlecase = student["name"].title()
     return students_titlecase
 
 
@@ -18,13 +18,10 @@ def add_student(name, student_id =332) :
     students.append(student)
 
 
-# varg Functions with *named *Variable Argument Lists
-def var_args(name, **kwargs):
-    print(name)
-    print(kwargs["description"],kwargs["feedback"] ,kwargs["pluralsight_Susbsccirber"] )
-
-
 student_list = get_students_titlecase()
 
-add_student("Mark")
-var_args("Mark", description="Love Python ", feedback= None, pluralsight_Susbsccirber= True)
+student_name = input("Enter student name: " )
+student_id = input("Enter student ID: " )
+
+add_student(student_name, student_id)
+print_students_titlecase()
