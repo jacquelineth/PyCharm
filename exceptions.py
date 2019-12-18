c93fb527eab12d36b55b6f35db931d99f39b11d4
jacquelineth <jacquelineth@gmail.com>
2019-12-18 15:59:20 +0100

    Multiple Excpetions - Type - General

--- exceptions.py
+++ exceptions.py
@@ -3,9 +3,18 @@ student = {
     "student_id": 15163,
     "feedback": None
 }
+
+student["last_name"] = "Kowalski"
+
 try:
     last_name= student["last_name"] 
+    numbered_last_name= 3 + last_name
 except KeyError:
     print("Error findng last_name")
-       
+except  TypeError:
+    print("I can't add these 2 together")
+except Exception:
+    print("Un")
+
+
 print("Finished")
