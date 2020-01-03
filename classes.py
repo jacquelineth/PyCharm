7b2e2ffcf2198d9a41be1274cddb3da31a34e782
Thierry JACQUELINE <jacqueline@gmail.com>
2020-01-04 00:47:24 +0100

    Class Attributes

--- classes.py
+++ classes.py
@@ -1,15 +1,26 @@
 students = []
 
 
-class Student:
-    def add_student(name, student_id =332) :
-    xstudent = {"name": name, "student_id": student_id}
-    students.append(xstudent)
+class Student: 
+    # Constructor
+    ### Need __init__  and self parameter
+    def __init__(self, name, student_id =332) :
+        xstudent = {"name": name, "student_id": student_id}
+        students.append(xstudent)
+        self.sName=name
+        self.__HName__= "private"
+    
+    # Public Attribut
+    sName = ""
 
-student = Student()
+    # private Attribut
+    __HName__ = ""
+
+    def __str__(self):
+        return super().__str__() + " Student :( " + self.sName + " )"
+
+student = Student("Mark")
 
 
 print(student)
-
-new_student = Student()
-print(new_student)
+print(student.__HName__)
